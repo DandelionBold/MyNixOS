5f4d6a4659eedd1e9d74ccb56666881582046df4
kamal nady <kamalnadykamal@gmail.com>
2025-10-06 20:12:04 +0300

    docs(hosts): add commented examples for nginx and DB toggles in server-01

--- hosts/server-01/default.nix
+++ hosts/server-01/default.nix
@@ -24,6 +24,27 @@
   #   enable = true;
   #   allowedTCPPorts = [ 22 80 443 ];
   # };
+
+  # Example: enable nginx with a simple HTTP site
+  # services.nginx = {
+  #   enable = true;
+  #   virtualHosts = {
+  #     "server-01.local" = {
+  #       root = "/var/www/server-01";
+  #       locations."/".extraConfig = "autoindex on;";
+  #     };
+  #   };
+  # };
+
+  # Example: enable MySQL (MariaDB) server
+  # services.mysql.enable = true;
+  # services.mysql.package = pkgs.mariadb;
+
+  # Example: enable Redis server
+  # services.redis.enable = true;
+
+  # Example: enable MSSQL server (EULA; additional channel required)
+  # services.mssql.enable = true;
 }
 
 
