b46ac0b0537cfda8f8c69dda90a25a9b66a35707
kamal nady <kamalnadykamal@gmail.com>
2025-10-08 00:17:29 +0300

    fix: correct import paths in VM personal from ../../ to ../../../ for features

--- hosts/vm/personal/personal.nix
+++ hosts/vm/personal/personal.nix
@@ -8,36 +8,36 @@
     ./hardware-configuration.nix
     
     # Desktop Environment
-    ../../features/desktop-environments/desktop-environment.nix
-    ../../features/desktop-environments/kde-plasma.nix
+    ../../../features/desktop-environments/desktop-environment.nix
+    ../../../features/desktop-environments/kde-plasma.nix
     
     # Development Tools
-    ../../features/development/dev.nix
-    ../../features/development/containers.nix
-    ../../features/development/databases.nix
+    ../../../features/development/dev.nix
+    ../../../features/development/containers.nix
+    ../../../features/development/databases.nix
     
     # Applications
-    ../../features/applications/browsers.nix
-    ../../features/applications/terminals.nix
-    ../../features/applications/file-managers.nix
-    ../../features/applications/gui-text-editors.nix
-    ../../features/applications/cli-text-editors.nix
-    ../../features/applications/screenshot-tools.nix
-    ../../features/applications/media-tools.nix
-    ../../features/applications/office-suite.nix
-    ../../features/applications/system-tools.nix
-    ../../features/applications/other-applications.nix
+    ../../../features/applications/browsers.nix
+    ../../../features/applications/terminals.nix
+    ../../../features/applications/file-managers.nix
+    ../../../features/applications/gui-text-editors.nix
+    ../../../features/applications/cli-text-editors.nix
+    ../../../features/applications/screenshot-tools.nix
+    ../../../features/applications/media-tools.nix
+    ../../../features/applications/office-suite.nix
+    ../../../features/applications/system-tools.nix
+    ../../../features/applications/other-applications.nix
     
     # Hardware Support
-    ../../features/hardware/bluetooth.nix
-    ../../features/hardware/printing.nix
-    ../../features/hardware/audio.nix
+    ../../../features/hardware/bluetooth.nix
+    ../../../features/hardware/printing.nix
+    ../../../features/hardware/audio.nix
     
     # System Features
-    ../../features/system/hibernate.nix
-    ../../features/system/power.nix
-    ../../features/system/themes/themes.nix
-    ../../features/system/themes/dark-theme.nix
+    ../../../features/system/hibernate.nix
+    ../../../features/system/power.nix
+    ../../../features/system/themes/themes.nix
+    ../../../features/system/themes/dark-theme.nix
   ];
   
   # Personal overrides for VM
