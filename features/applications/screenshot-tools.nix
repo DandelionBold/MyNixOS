615cc33934c8d8142d0d4af973b1b9ee24703a7e
kamal nady <kamalnadykamal@gmail.com>
2025-10-08 21:55:49 +0300

    fix(apps): replace deprecated spectacle with kdePackages.spectacle (Qt6)

--- features/applications/screenshot-tools.nix
+++ features/applications/screenshot-tools.nix
@@ -2,7 +2,8 @@
 
 {
   # Screenshot and screen recording tools
+  # Spectacle moved to Qt6; use kdePackages.spectacle
   environment.systemPackages = with pkgs; [
-    spectacle
+    kdePackages.spectacle
   ];
 }
