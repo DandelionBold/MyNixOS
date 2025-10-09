ff6ab065c05db37348e658fa92c481fb13c70856
kamal nady <kamalnadykamal@gmail.com>
2025-10-09 20:20:57 +0300

    fix: comment out MSSQL Docker container due to startup issues

--- features/development/databases.nix
+++ features/development/databases.nix
@@ -12,19 +12,23 @@
   # Redis
   services.redis.enable = lib.mkDefault true;
 
-  # MSSQL via Docker container (SQL Server 2022)
-  virtualisation.oci-containers = {
-    backend = "docker";
-    containers.mssql = {
-      image = "mcr.microsoft.com/mssql/server:2022-latest";
-      ports = [ "1433:1433" ];
-      environment = {
-        ACCEPT_EULA = "Y";
-        MSSQL_SA_PASSWORD = "ChangeMe123!"; # change me or wire via secrets
-      };
-      extraOptions = [ "--restart=unless-stopped" ];
-    };
-  };
+  # MSSQL via Docker container (SQL Server 2022) - COMMENTED OUT DUE TO STARTUP ISSUES
+  # virtualisation.oci-containers = {
+  #   backend = "docker";
+  #   containers.mssql = {
+  #     image = "mcr.microsoft.com/mssql/server:2022-latest";
+  #     ports = [ "11433:1433" ];  # host:container (changed host port)
+  #     environment = {
+  #       ACCEPT_EULA = "Y";
+  #       MSSQL_SA_PASSWORD = "ChangeMe123!"; # pick your own
+  #       MSSQL_PID = "Developer";
+  #     };
+  #     volumes = [ "/var/lib/mssql:/var/opt/mssql" ];
+  #     extraOptions = [ "--restart=unless-stopped" ];
+  #   };
+  # };
+  # # make sure the volume exists
+  # systemd.tmpfiles.rules = [ "d /var/lib/mssql 0750 root root -" ];
 }
 
 
