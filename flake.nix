7d53d7adb2f8c0beff2cd06eb74845d260a87f19
kamal nady <kamalnadykamal@gmail.com>
2025-10-09 16:55:06 +0300

    feat: add hydra.nixos.org substituter and trusted public key for more pre-built packages

--- flake.nix
+++ flake.nix
@@ -6,9 +6,11 @@
     # Prefer well-known binary caches for faster builds
     substituters = [
       "https://cache.nixos.org/"
+      "https://hydra.nixos.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypBMrAURkbJ16zPMQFGspcDShjY="
+      "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
     ];
     experimental-features = [ "nix-command" "flakes" ];
   };
