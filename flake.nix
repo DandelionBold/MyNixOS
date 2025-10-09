7f3d77a491570a190ab0511f1f7742115f065abd
kamal nady <kamalnadykamal@gmail.com>
2025-10-09 16:57:38 +0300

    fix: correct cache.nixos.org trusted public key format (uppercase Z)

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       "https://hydra.nixos.org"
     ];
     trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypBMrAURkbJ16zPMQFGspcDShjY="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
     ];
     experimental-features = [ "nix-command" "flakes" ];
