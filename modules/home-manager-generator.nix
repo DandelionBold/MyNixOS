0c8ccf2e7001508981bb02990eee462e2c956bab
kamal nady <kamalnadykamal@gmail.com>
2025-10-10 16:53:42 +0300

    fix: correct mkHomeConfig function to return configuration directly instead of {name, value} object

--- modules/home-manager-generator.nix
+++ modules/home-manager-generator.nix
@@ -6,57 +6,54 @@ let
   users = usersData.users;
   
   # Helper to create Home Manager configuration for a user
-  mkHomeConfig = username: user: {
-    name = username;
-    value = home-manager.lib.homeManagerConfiguration {
-      pkgs = pkgs;
-      extraSpecialArgs = { };
-      modules = 
-        # Import extra modules if defined
-        (user.hm.extraModules or []) ++
-        # Base configuration module
-        [
-          {
-            home.username = username;
-            home.homeDirectory = user.homeDirectory;
-            home.stateVersion = "24.05";
-            
-            # Import user's HM configuration
-            imports = user.hm.imports or [];
-            
-            # Theme configuration
-            theme = user.hm.theme or { enable = false; };
-            
-            # Bash configuration (if defined)
-            programs.bash = if (user.hm ? bash) then (user.hm.bash // {
-              enable = user.hm.bash.enable or true;
-            }) else {
-              enable = false;
-            };
-            
-            # Git configuration (if defined)
-            programs.git = if (user.hm ? git) then (user.hm.git // {
-              enable = user.hm.git.enable or true;
-            }) else {
-              enable = false;
-            };
-            
-            # Vim configuration (if defined)
-            programs.vim = if (user.hm ? vim) then (user.hm.vim // {
-              enable = user.hm.vim.enable or false;
-            }) else {
-              enable = false;
-            };
-            
-            # Zsh configuration (if defined)
-            programs.zsh = if (user.hm ? zsh) then (user.hm.zsh // {
-              enable = user.hm.zsh.enable or true;
-            }) else {
-              enable = false;
-            };
-          }
-        ];
-    };
+  mkHomeConfig = username: user: home-manager.lib.homeManagerConfiguration {
+    pkgs = pkgs;
+    extraSpecialArgs = { };
+    modules = 
+      # Import extra modules if defined
+      (user.hm.extraModules or []) ++
+      # Base configuration module
+      [
+        {
+          home.username = username;
+          home.homeDirectory = user.homeDirectory;
+          home.stateVersion = "24.05";
+          
+          # Import user's HM configuration
+          imports = user.hm.imports or [];
+          
+          # Theme configuration
+          theme = user.hm.theme or { enable = false; };
+          
+          # Bash configuration (if defined)
+          programs.bash = if (user.hm ? bash) then (user.hm.bash // {
+            enable = user.hm.bash.enable or true;
+          }) else {
+            enable = false;
+          };
+          
+          # Git configuration (if defined)
+          programs.git = if (user.hm ? git) then (user.hm.git // {
+            enable = user.hm.git.enable or true;
+          }) else {
+            enable = false;
+          };
+          
+          # Vim configuration (if defined)
+          programs.vim = if (user.hm ? vim) then (user.hm.vim // {
+            enable = user.hm.vim.enable or false;
+          }) else {
+            enable = false;
+          };
+          
+          # Zsh configuration (if defined)
+          programs.zsh = if (user.hm ? zsh) then (user.hm.zsh // {
+            enable = user.hm.zsh.enable or true;
+          }) else {
+            enable = false;
+          };
+        }
+      ];
   };
   
 in
